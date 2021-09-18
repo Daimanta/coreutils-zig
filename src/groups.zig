2b67b6bdae8978486b4db965516171540a35aa89
Léon van der Kaap <leonkaap@gmail.com>
2021-09-18 15:35:22 +0200

    Finished provisional version of 'groups'.

--- src/groups.zig
+++ src/groups.zig
@@ -5,6 +5,7 @@ const mem = std.mem;
 const uid = linux.uid_t;
 const gid = linux.gid_t;
 const users = @import("util/users.zig");
+const Allocator = std.mem.Allocator;
 
 const allocator = std.heap.page_allocator;
 
@@ -33,16 +34,19 @@ pub fn main() !void {
 
     var current_mode: ?Mode = null;
     if (arguments.len > 2) {
-        std.debug.print("{s}: too many arguments", .{application_name});
-        std.os.exit(1);
+        current_mode = Mode.main;
     } else if (arguments.len == 2) {
         if (mem.eql(u8, "--help", arguments[1])) {
             current_mode = Mode.help;
         } else if (mem.eql(u8, "--version", arguments[1])) {
             current_mode = Mode.version;
         } else {
-            std.debug.print("{s}: Unknown argument \"{s}\"\n", .{application_name, arguments[1]});
-            std.os.exit(1);
+            if (arguments[1].len > 0 and arguments[1][0] == '-') {
+                std.debug.print("{s}: Unknown argument \"{s}\"\n", .{application_name, arguments[1]});
+                std.os.exit(1);
+            } else {
+                current_mode = Mode.main;
+            }
         }
     } else {
         current_mode = Mode.main;
@@ -64,9 +68,19 @@ pub fn main() !void {
         if (count == 0) {
             const my_uid = linux.geteuid();
             const pw: *users.Passwd = users.getpwuid(my_uid);
-            try display_group(pw.pw_name, pw.pw_gid);
+            try display_group(pw, false);
         } else {
-
+            for(arguments[1..]) |argument| {
+                if (argument.len > 0 and argument[0] != '-') {
+                    var user_null_pointer = try to_null_terminated_pointer(argument, allocator);
+                    defer allocator.free(user_null_pointer);
+                    if (users.get_user_by_name(user_null_pointer)) |pw| {
+                        try display_group(pw, true);
+                    } else |err| {
+                        std.debug.print("{s}: '{s}': no such user\n", .{application_name, argument});
+                    }
+                }
+            }
         }
 
         std.os.exit(0);
@@ -77,26 +91,43 @@ pub fn main() !void {
 
 }
 
-pub fn display_group (user: [*:0]const u8, my_gid: gid) !void {
-    var user_gid: gid = my_gid;
+fn display_group (user: *users.Passwd, print_name: bool) !void {
+    var user_gid: gid = user.pw_gid;
     var groups: [*]gid = undefined;
     var group_count: c_int = 0;
-    var size_iteration = getgrouplist(user, my_gid, groups, &group_count);
+
+    // Size iteration
+    _ = getgrouplist(user.pw_name, user_gid, groups, &group_count);
     var group_count_usize = @intCast(usize, group_count);
-    std.debug.print("{d}\n", .{group_count_usize});
     var group_alloc = try allocator.alloc(gid, group_count_usize);
     groups = group_alloc.ptr;
-    var data_iteration = getgrouplist(user, my_gid, groups, &group_count);
-    std.debug.print("{d}\n", .{group_count});
-    for (groups[0..@intCast(usize, group_count)]) |group| {
-        std.debug.print("{d}\n", .{group});
+    defer allocator.free(group_alloc);
+    // Actually allocate the groups
+    _ = getgrouplist(user.pw_name, user_gid, groups, &group_count);
+
+    if (print_name) {
+        std.debug.print("{s} : ", .{user.pw_name});
     }
+    for (groups[0..@intCast(usize, group_count)]) |group| {
+        const grp = users.getgrgid(group);
+        std.debug.print("{s} ", .{grp.gr_name});
+    }
+    std.debug.print("\n", .{});
 }
 
-pub fn null_pointer_length(ptr: [*:0]u8) usize {
+fn null_pointer_length(ptr: [*:0]const u8) usize {
     var result: usize = 0;
     while (ptr[result] != 0) {
         result += 1;
     }
     return result;
+}
+
+fn to_null_terminated_pointer(slice: []const u8, allocator_impl: *Allocator) ![:0]u8 {
+    var result = try allocator_impl.alloc(u8, slice.len + 1);
+    for (slice) |byte, i| {
+        result[i] = slice[i];
+    }
+    result[result.len - 1] = 0;
+    return result[0..result.len - 1:0];
 }
--- src/util/users.zig
+++ src/util/users.zig
@@ -7,10 +7,31 @@ const gid = linux.gid_t;
 
 pub const Passwd = extern struct {
     pw_name: [*:0]u8,
+    pw_passwd: [*:0]u8,
     pw_uid: uid,
     pw_gid: gid,
+    pw_gecos: [*:0]u8,
     pw_dir: [*:0]u8,
     pw_shell: [*:0]u8
 };
 
+pub const Group = extern struct {
+    gr_name: [*:0]u8,
+    gr_passwd: [*:0]u8,
+    gr_gid: gid,
+    gr_mem: [*][*:0]u8
+};
+
 pub extern fn getpwuid (uid: uid) callconv(.C) *Passwd;
+pub extern fn getpwnam (name: [*:0]u8) callconv(.C) *Passwd;
+
+pub fn get_user_by_name(name: [*:0]u8) !*Passwd {
+    const result = getpwnam(name);
+    if (@ptrToInt(result) == 0) {
+        return error.UserNotFound;
+    } else {
+        return result;
+    }
+}
+
+pub extern fn getgrgid (gid: gid) callconv(.C) *Group;
