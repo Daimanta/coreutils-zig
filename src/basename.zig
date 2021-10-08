b23b653806dfddd415c542dec959bad6a6a12c70
Léon van der Kaap <leonkaap@gmail.com>
2021-10-08 19:25:53 +0200

    Started on 'basename'

---
+++ src/basename.zig
@@ -0,0 +1,117 @@
+const std = @import("std");
+const fs = std.fs;
+const os = std.os;
+const linux = os.linux;
+
+const clap = @import("clap.zig");
+const fileinfo = @import("util/fileinfo.zig");
+const strings = @import("util/strings.zig");
+const version = @import("util/version.zig");
+
+const Allocator = std.mem.Allocator;
+
+const allocator = std.heap.page_allocator;
+const kernel_stat = linux.kernel_stat;
+
+const application_name = "basename";
+const help_message =
+    \\Usage: basename NAME [SUFFIX]
+    \\  or:  basename OPTION... NAME...
+    \\Print NAME with any leading directory components removed.
+    \\If specified, also remove a trailing SUFFIX.
+    \\
+    \\Mandatory arguments to long options are mandatory for short options too.
+    \\  -a, --multiple       support multiple arguments and treat each as a NAME
+    \\  -s, --suffix=SUFFIX  remove a trailing SUFFIX; implies -a
+    \\  -z, --zero           end each output line with NUL, not newline
+    \\      --help     display this help and exit
+    \\      --version  output version information and exit
+    \\
+    \\Examples:
+    \\  basename /usr/bin/sort          -> "sort"
+    \\  basename include/stdio.h .h     -> "stdio"
+    \\  basename -s .h include/stdio.h  -> "stdio"
+    \\
+    \\
+;
+
+pub fn main() !void {
+    const params = comptime [_]clap.Param(clap.Help){
+        clap.parseParam("--help") catch unreachable,
+        clap.parseParam("--version") catch unreachable,
+        clap.parseParam("-a, --multiple") catch unreachable,
+        clap.parseParam("-s, --suffix <SUFF>") catch unreachable,
+        clap.parseParam("-z, --zero") catch unreachable,
+        clap.parseParam("<STRING>") catch unreachable,
+    };
+
+    var diag = clap.Diagnostic{};
+    var args = clap.parseAndHandleErrors(clap.Help, &params, .{ .diagnostic = &diag }, application_name, 1);
+
+    var resolve_symlink = false;
+
+    if (args.flag("--help")) {
+        std.debug.print(help_message, .{});
+        std.os.exit(0);
+    } else if (args.flag("--version")) {
+        version.printVersionInfo(application_name);
+        std.os.exit(0);
+    }
+
+    var multiple = (args.flag("-a") or args.flag("--multiple"));
+    const suffix = args.option("-s");
+    if (suffix != null) multiple = true;
+    const zero = (args.flag("-z") or args.flag("--zero"));
+    const newline = if (zero) "\x00" else "\n";
+
+    const positionals = args.positionals();
+    if (positionals.len == 0) {
+        std.debug.print("{s}: missing operand\n", .{application_name});
+        std.os.exit(1);
+    } else if (positionals.len > 2 and !multiple) {
+        std.debug.print("{s}: Only name and suffix expected\n", .{application_name});
+        std.os.exit(1);
+    }
+
+    for (positionals) |pos| {
+        processFile(pos, suffix, newline);
+    }
+}
+
+fn processFile(file: []const u8, suffix: ?[]const u8, newline: []const u8) void {
+    var first_char: usize = undefined;
+    var last_char: usize = undefined;
+    var last_slash: ?usize = null;
+    if (file[file.len - 1] == '/') {
+        var last_non_slash: ?usize = null;
+        var i: isize = @intCast(isize, file.len - 1);
+        while (i >= 0) : (i -= 1) {
+            if (file[@intCast(usize, i)] != '/') {
+                last_non_slash = @intCast(usize, i);
+                break;
+            }
+        }
+        if (last_non_slash == null) {
+            std.debug.print("/{s}", .{newline});
+            return;
+        } else {
+            last_char = last_non_slash.?;
+            var next_slash: ?usize = null;
+            i = @intCast(isize, last_char);
+            while (i >= 0) : (i -= 1) {
+                if (file[@intCast(usize, i)] == '/') {
+                    next_slash = @intCast(usize, i);
+                    break;
+                }
+            }
+            if (next_slash == null) first_char = 0 else first_char = next_slash.? + 1;
+            stripSuffix(file[first_char..last_char+1], suffix, newline);
+        }
+    } else {
+        
+    }
+}
+
+fn stripSuffix(string: []const u8, suffix: ?[]const u8, newline: []const u8) void {
+    std.debug.print("{s}{s}", .{string, newline});
+}
