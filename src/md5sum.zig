a96764707bd6f720799dc0d2e89e9ad351c6158e
Léon van der Kaap <leonkaap@gmail.com>
2022-01-30 23:07:11 +0100

    Did more work on 'md5sum'.

--- src/md5sum.zig
+++ src/md5sum.zig
@@ -53,6 +53,8 @@ const help_message =
 
 var handled_stdin = false;
 
+const HashError = error{ FileDoesNotExist, IsDir, FileAccessFailed, OtherError };
+
 pub fn main() !void {
     const params = comptime [_]clap.Param(clap.Help){
         clap.parseParam("--help") catch unreachable,
@@ -87,50 +89,105 @@ pub fn main() !void {
     const zero_terminated = args.flag("-z");
     const ignore_missing = args.flag("--ignore-missing");
     const quiet = args.flag("--quiet");
-    const status = args.flag("--status");
+    const status_only = args.flag("--status");
     const strict = args.flag("--strict");
     const warn = args.flag("-w");
 
     const terminator: []const u8 = if (zero_terminated) "\x00" else "\n";
 
-    if (!check and (ignore_missing or quiet or status or strict or warn)) {
+    if (!check and (ignore_missing or quiet or status_only or strict or warn)) {
         print("When '--check' is not set, non-check flags may not be active.\n", .{});
         std.os.exit(1);
     }
 
     const positionals = args.positionals();
+    var clean = true;
     if (check) {
         for (positionals) |arg| {
-            checkFile(arg, ignore_missing, quiet, status, strict, warn);
+            clean = checkFile(arg, ignore_missing, quiet, status_only, strict, warn) and clean;
         }
     } else {
         for (positionals) |arg| {
-            hashFile(arg, bsd, terminator);
+            clean = hashFile(arg, bsd, terminator) and clean;
         }
+    }
+    if (!clean) {
+        std.os.exit(1);
     }
 }
 
-fn checkFile(path: []const u8, ignore_missing: bool, quiet: bool, status: bool, strict: bool, warn: bool) void {
-    _ = path;
-    _ = ignore_missing;
-    _ = quiet;
-    _ = status;
-    _ = strict;
-    _ = warn;
-    if (mem.eql(u8, "-while ", path)) {
-        if (!handled_stdin) {
-            handled_stdin = true;
-        }
-    }
-}
-
-fn hashFile(path: []const u8, bsd: bool, terminator: []const u8) void {
+fn checkFile(path: []const u8, ignore_missing: bool, quiet: bool, status: bool, strict: bool, warn: bool) bool {
     if (mem.eql(u8, "-", path)) {
         if (!handled_stdin) {
             const stdin = std.io.getStdIn().reader();
             const bytes = stdin.readAllAlloc(default_allocator, 1 << 30) catch {
                 std.debug.print("Reading stdin failed\n", .{});
-                return;
+                return false;
+            };
+            defer default_allocator.free(bytes);            
+            handled_stdin = true;
+            return checkBytes(bytes, ignore_missing, quiet, status, strict, warn);
+        }
+    } else {
+        const stat = fileinfo.getLstat(path) catch |err| {
+            print("{s}\n", .{err});
+            return false;
+        };
+        if (!fileinfo.fileExists(stat)) {
+            print("{s}: File '{s}' does not exist\n", .{ application_name, path });
+            return false;
+        }
+
+        if (fileinfo.isDir(stat)) {
+            print("{s}: '{s}' is a directory.\n", .{ application_name, path });
+            return false;
+        }
+
+        const file = fs.cwd().openFile(path, .{ .read = true }) catch {
+            print("Could not read file.\n", .{});
+            return false;
+        };
+        defer file.close();
+        const bytes = file.readToEndAlloc(default_allocator, 1 << 30) catch return false;        
+        defer default_allocator.free(bytes);
+        return checkBytes(bytes, ignore_missing, quiet, status, strict, warn);
+    }
+    return true;
+
+}
+
+fn checkBytes(bytes: []const u8, ignore_missing: bool, quiet: bool, status_only: bool, strict: bool, warn: bool) bool {
+    _ = bytes;
+    _ = quiet;
+    _ = warn;
+    var result = true;
+    var missing: u32 = 0;
+    var incorrect: u32 = 0;
+    var format: u32 = 0;
+    if ((missing > 0 and !ignore_missing) or incorrect > 0 or (format > 0 and strict)) {
+        result = false;
+    }
+    if (missing > 0 and !status_only and !ignore_missing) {
+        print("{s}: WARNING: {d} files could not be read\n", .{application_name, missing});
+    }
+
+    if (incorrect > 0 and !status_only) {
+        print("{s}: WARNING: {d} hashes did not match\n", .{application_name, missing});
+    }
+
+    if (format > 0 and !status_only) {
+        print("{s}: WARNING: {d} lines are improperly formatted\n", .{application_name, missing});
+    }
+    return result;
+}
+
+fn hashFile(path: []const u8, bsd: bool, terminator: []const u8) bool {
+    if (mem.eql(u8, "-", path)) {
+        if (!handled_stdin) {
+            const stdin = std.io.getStdIn().reader();
+            const bytes = stdin.readAllAlloc(default_allocator, 1 << 30) catch {
+                std.debug.print("Reading stdin failed\n", .{});
+                return false;
             };
             var hash_result: [HASH_BYTE_SIZE]u8 = undefined;
             Md5.hash(bytes, &hash_result, .{});
@@ -144,47 +201,53 @@ fn hashFile(path: []const u8, bsd: bool, terminator: []const u8) void {
             handled_stdin = true;
         }
     } else {
-        const stat = fileinfo.getLstat(path) catch |err| {
-            print("{s}\n", .{err});
-            return;
-        };
-        if (!fileinfo.fileExists(stat)) {
-            print("{s}: File '{s}' does not exist\n", .{ application_name, path });
-            return;
-        }
-
-        if (fileinfo.isDir(stat)) {
-            print("{s}: '{s}' is a directory.\n", .{ application_name, path });
-            return;
-        }
-
-        const file_size = @intCast(u64, stat.size);
-
-        const file = fs.cwd().openFile(path, .{ .read = true }) catch {
-            print("Could not read file.\n", .{});
-            return;
-        };
-        defer file.close();
-
-        var offset: usize = 0;
-        var file_buffer = default_allocator.alloc(u8, block_read) catch return;
-        defer default_allocator.free(file_buffer);
-        var hash_processor = Md5.init(.{});
-        while (offset < file_size) {
-            const read = file.pread(file_buffer[0..], offset) catch return;
-            hash_processor.update(file_buffer[0..read]);
-            offset += block_read;
-        }
-        var hash_result: [HASH_BYTE_SIZE]u8 = undefined;
-        hash_processor.final(&hash_result);
-        var hash_string: [2 * HASH_BYTE_SIZE]u8 = undefined;
-        digest_to_hex_string(&hash_result, &hash_string);
+        const hash_string = digestFromFile(path) catch unreachable;
         if (bsd) {
             print("MD5 ({s}) = {s}{s}", .{ path, hash_string, terminator });
         } else {
             print("{s}  {s}{s}", .{ hash_string, path, terminator });
         }
     }
+    return true;
+}
+
+fn digestFromFile(path: []const u8) HashError![2 * HASH_BYTE_SIZE]u8 {
+    const stat = fileinfo.getLstat(path) catch |err| {
+        print("{s}\n", .{err});
+        return HashError.OtherError;
+    };
+    if (!fileinfo.fileExists(stat)) {
+        print("{s}: File '{s}' does not exist\n", .{ application_name, path });
+        return HashError.FileDoesNotExist;
+    }
+
+    if (fileinfo.isDir(stat)) {
+        print("{s}: '{s}' is a directory.\n", .{ application_name, path });
+        return HashError.IsDir;
+    }
+
+    const file_size = @intCast(u64, stat.size);
+
+    const file = fs.cwd().openFile(path, .{ .read = true }) catch {
+        print("Could not read file.\n", .{});
+        return HashError.FileAccessFailed;
+    };
+    defer file.close();
+
+    var offset: usize = 0;
+    var file_buffer = default_allocator.alloc(u8, block_read) catch return HashError.OtherError;
+    defer default_allocator.free(file_buffer);
+    var hash_processor = Md5.init(.{});
+    while (offset < file_size) {
+        const read = file.pread(file_buffer[0..], offset) catch return HashError.OtherError;
+        hash_processor.update(file_buffer[0..read]);
+        offset += block_read;
+    }
+    var hash_result: [HASH_BYTE_SIZE]u8 = undefined;
+    hash_processor.final(&hash_result);
+    var hash_string: [2 * HASH_BYTE_SIZE]u8 = undefined;
+    digest_to_hex_string(&hash_result, &hash_string);
+    return hash_string;
 }
 
 fn digest_to_hex_string(digest: *[HASH_BYTE_SIZE]u8, string: *[2 * HASH_BYTE_SIZE]u8) void {
