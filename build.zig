dcf9028404ce1700c2cb34bbc5dec5a1ce005c20
Léon van der Kaap <leonkaap@gmail.com>
2025-08-22 22:15:33 +0200

    Disabled Zig 0.14 compat

--- build.zig
+++ build.zig
@@ -5,8 +5,8 @@ const Builder = std.Build;
 
 pub fn build(b: *Builder) void {
     const current_zig_version = @import("builtin").zig_version;
-    if (current_zig_version.major != 0 or current_zig_version.minor < 14) {
-        std.debug.print("This project does not compile with a Zig version <0.14.x. Exiting.", .{});
+    if (current_zig_version.major != 0 or current_zig_version.minor < 15) {
+        std.debug.print("This project does not compile with a Zig version <0.15.x. Exiting.", .{});
         std.os.exit(1);
     }
 
